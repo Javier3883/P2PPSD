e1b003153cba5195778c231156ca6465ce5d0f04
Javier3883 <j.gonzalezn@udc.es>
2026-03-23 16:16:13 +0100

    subir

---
+++ monoalfafeto.py
@@ -0,0 +1,97 @@
+
+import sys
+if "--help" in sys.argv or "-h" in sys.argv:
+    print(
+        "Este programa implementa un cifrado monoalfabético con un pseudoaleatorio basado en una semilla proporcionada por el usuario.\n"
+        "Opciones de uso:\n"
+        "1. Ingrese un número entero para la semilla cuando se le solicite.\n"
+        "2. El programa generará un diccionario de sustitución basado en el texto 'MONOALFABETO' y la semilla proporcionada.\n"
+        "3. El resultado del cifrado se mostrará al final junto con el diccionario de sustitución ordenado.\n\n"
+        "Nota: El texto a cifrar está predefinido como 'MONOALFABETO', pero puede modificarse en la variable 'texto'.\n"
+        "El programa utiliza un método de generación de números pseudoaleatorios para asignar letras del alfabeto a otras letras.\n"
+        "El programa asegura que no haya repeticiones en las asignaciones de letras.\n"
+        "El usuario debe ingresar un número entero para la semilla.\n"
+        "El programa muestra el diccionario de sustitución ordenado alfabéticamente.\n"
+        "El resultado final del cifrado se muestra al final del programa.\n")
+    sys.exit()
+
+ 
+if len(sys.argv) > 1:
+    config_file = sys.argv[1]
+    try:
+        with open(config_file, "r", encoding="utf-8") as f:
+            texto = f.read()
+        print(f"\nCifrando configuración de: {config_file}\n")
+    except FileNotFoundError:
+        print(f"Error: El archivo '{config_file}' no existe.", file=sys.stderr)
+        sys.exit()
+
+else:
+    texto = "MONOALFABETO"
+    print("\n Cifrando texto de: MONOALFABETO \n")
+
+
+
+
+
+def cifrado_monoalfabetico(texto):
+    texto = texto.upper()
+    alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    a = 4825785162
+    c = int(input("Ingrese un número entero para la semilla: ")) * 857412552
+    m = 15**32
+    semilla = len(texto) * 38
+
+    letras_texto = []
+    for letra in texto:
+        if letra in alfabeto and letra not in letras_texto:
+            letras_texto.append(letra) # añade cada letra única que esté en el alfabeto, sin repetir.
+
+    for letra in alfabeto:
+        if letra not in letras_texto:
+            letras_texto.append(letra) # añade las letras del alfabeto que no están en el texto, para comprobar un funcionamiento correcto de monoalfabeto
+
+    dic = {} # Diccionario para almacenar las asignaciones de letras
+    usadas = []  # lista del texto ya asignadas
+
+    i = 0  
+
+    while i < 26:
+        letra_alf = alfabeto[i] # letra actual del alfabeto 
+        semilla = (a * semilla + 77 * c) % m # número pseudoaleatorio
+        idx_txt = semilla % len(letras_texto) # convertir número a un índice valido
+        letra_txt = letras_texto[idx_txt] # letra candidata a sustituir
+
+        while letra_txt in usadas: # si la letra ya fue usada, generar número pseudoaleatorio hasta encontrar una letra no usada
+            if len(usadas) == len(letras_texto):
+                break
+            semilla = (a * semilla + 77 * c) % m
+            idx_txt = semilla % len(letras_texto)
+            letra_txt = letras_texto[idx_txt]
+        
+        if len(usadas) == len(letras_texto): # Si todas las letras han sido usadas, salir del bucle
+            break
+
+        dic[letra_alf] = letra_txt 
+        usadas.append(letra_txt)
+
+        i += 1
+
+  
+    resultado = ""
+    for letra in texto:
+        if letra in dic:
+            resultado += dic[letra]
+
+
+    print("\nDiccionario de sustitución (ordenado A → Z):")
+    for clave in sorted(dic.keys()):
+        print(clave, "--", dic[clave])
+
+    return resultado
+
+
+cifrado = cifrado_monoalfabetico(texto)
+
+print("\nTexto cifrado:\n", cifrado)
---
+++ vigenere.py
@@ -0,0 +1,65 @@
+with open("1.5_fragmentoPrueba.txt", "r", encoding="utf-8") as f:
+    prueba = f.read()
+
+
+def matriz_vigenere():
+    alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    matriz = []
+
+    for i in range(len(alfabeto)):
+        fila = ""
+        for j in range(len(alfabeto)):
+            letra = alfabeto[(i + j) % len(alfabeto)]
+            fila += letra + " "
+        matriz.append(fila)
+
+    return matriz
+
+
+def clave():
+    clave = input("Ingrese la clave para un cifrado de Vigenere: ")
+    clave = clave.upper()
+    if len(clave) == 0:
+        print("La clave no puede estar vacía ")
+        return clave()
+    elif len(clave) > 7:
+        print("La clave es demasiado larga")
+        return clave()
+    return clave
+
+
+def programa(texto_cifrar):
+    matrix_completa = matriz_vigenere()
+    clave_llamada = clave()
+    resultado_cifrado = ""
+    
+    for i in range(len(texto_cifrar)):
+        letra_t = texto_cifrar[i]
+        indice_columna = matrix_completa[0].index(letra_t)
+        letra_clave = clave_llamada[i % len(clave_llamada)]
+        indice_fila = 0
+        for i in range(len(matrix_completa)):
+            if matrix_completa[i][0] == letra_clave:
+                indice_fila = i
+                break
+        
+        # LETRA CIFRADA
+        letra_cifrada = matrix_completa[indice_fila][indice_columna]
+        resultado_cifrado += letra_cifrada
+
+    return resultado_cifrado 
+
+matriz = matriz_vigenere()
+
+
+print("Matriz de Vigenere: ")
+for fila in matriz:
+    print(fila)
+
+print("\n")
+print("Algoritmo cifrado: ")
+print("\n")
+print(programa(prueba))
+
+
+
